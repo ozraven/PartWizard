c47692f61049ae5d89040eb24abc376f2bdda2d3
Eric Harris <ozraven@gmail.com>
2016-04-20 10:39:12 -0400

    Update for 1.1.

--- PartWizard/Properties/AssemblyInfo.cs
+++ PartWizard/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Part Wizard")]
-[assembly: AssemblyCopyright("Copyright © 2014-2015 Eric Harris (ozraven)")]
+[assembly: AssemblyCopyright("Copyright © 2014-2016 Eric Harris (ozraven)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,8 +34,8 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.3.0")]
-[assembly: AssemblyFileVersion("1.2.3.0")]
+[assembly: AssemblyVersion("1.2.4.0")]
+[assembly: AssemblyFileVersion("1.2.4.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 
 [assembly: KSPAssembly("PartWizard", 1, 2)]
